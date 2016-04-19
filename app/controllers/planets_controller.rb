3772e7c7bdc1088bf42093a6d8193556bc6f7eb0
Mia Gaulin <gaulin.mia@gmail.com>
2016-04-19 19:02:18 -0400

    api integration for planets

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,5 +5,15 @@ class PlanetsController < ApplicationController
 
   def show
     @planet = Planet.find(params[:id])
+    @planet_data = api_parse
+  end
+
+  def api_parse
+    @planet = Planet.find(params[:id])
+    apicall = Swapi.get_planet @planet.id
+    a = apicall.gsub(", ", "&")
+    b = a.gsub(",", "//")
+    c = b.gsub("&", ", ")
+    d = c.gsub(/[{}\"]/,'').split('//').map{|h| h1,h2 = h.split(':'); {h1 => h2}}.reduce(:merge)
   end
 end
